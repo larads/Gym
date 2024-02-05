ea9bbff3b9591d64baa5faa35d742861b7a04a48
Mariana da Silva <dasilval.mariana@gmail.com>
2024-02-04 22:12:11 -0300

    🌈 style(customizing the theme):

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import { View, StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading';
 
+import { Theme } from 'src/theme';
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={Theme}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
