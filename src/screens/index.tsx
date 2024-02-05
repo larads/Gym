78e0248df1627a8fde22b6f794587fdd12c15781
Mariana da Silva <dasilval.mariana@gmail.com>
2024-02-04 23:43:52 -0300

    🦄 refactor(finishing  SingIn):

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, View, Heading} from "native-base"
+import { VStack, Image, Text, Center, View, Heading, ScrollView} from "native-base"
 import { MaterialIcons } from '@expo/vector-icons';
 import BackgroundImg  from "@assets/background.png"
 import { Input } from "@components/Input";
@@ -6,42 +6,48 @@ import { Button } from "@components/Button";
 
 export function SignIn() {
     return(
-        <VStack flex={1} bg="gray.700" px={10}>
-            <Image 
-                source={BackgroundImg} 
-                alt=""
-                resizeMode="contain"
-                position="absolute"
-            />
-            <Center my={24}>
-                <View style={{ flexDirection: "row", alignSelf: "center" }}>
-                    <MaterialIcons name="fitness-center" size={30} color="#00875F"/>
-                    <Heading color={"white"} marginLeft={2}>Gym</Heading>
-                </View>
+        <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
+            <VStack flex={1} bg="gray.700" px={10} pb={20}>
+                <Image 
+                    source={BackgroundImg} 
+                    alt=""
+                    resizeMode="contain"
+                    position="absolute"
+                />
+                <Center my={24}>
+                    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+                        <MaterialIcons name="fitness-center" size={30} color="#00875F"/>
+                        <Heading color={"white"} marginLeft={2}>Gym</Heading>
+                    </View>
 
-                <Text color="gray.200" bold fontSize="sm">Treine sua Mente e Corpo</Text>
-            </Center>
-            <Center>
-                <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-                    Acesse sua Conta
-                </Heading>
-            </Center>
+                    <Text color="gray.200" bold fontSize="sm">Treine sua Mente e Corpo</Text>
+                </Center>
+                <Center>
+                    <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+                        Acesse sua Conta
+                    </Heading>
+            
+                    <Input 
+                        placeholder="E-mail"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                        />
 
-                            
-            <Input 
-                placeholder="E-mail"
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
+                    <Input 
+                        placeholder="Senha"
+                        secureTextEntry
+                        />
 
-            <Input 
-                placeholder="Senha"
-                secureTextEntry
-            />
+                    <Button title="Acessar"/>
+                </Center>
 
-            <Button title="Acessar"/>
-
-            <Button title="Criar Conta" variant="outline" />
-        </VStack>
+                <Center mt={24}>
+                    <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+                        Ainda não tem acesso?
+                    </Text>
+                    <Button title="Criar Conta" variant="outline" />
+                </Center>
+            </VStack>
+        </ScrollView>
     )
 }
