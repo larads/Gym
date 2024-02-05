0106f5d56e1efc8014176856fc742b3a5cbe9c5f
Mariana da Silva <dasilval.mariana@gmail.com>
2024-02-04 21:31:45 -0300

    🌈 style(Customizing the StatusBar):

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {fontsLoaded ? <Text style={{ fontFamily: 'Roboto_700Bold'}}>Helou</Text> : <View />}
     </View>
   );
