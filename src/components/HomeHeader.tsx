fbfd88e3766ac2f9563ed8a0726e15086d20da9c
Mariana da Silva <dasilval.mariana@gmail.com>
2024-02-06 00:40:04 -0300

    🦄 refactor(component Group):

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,10 +1,17 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Text, HStack, Heading, VStack, Center } from "native-base";
 import { TouchableOpacity } from "react-native";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
     return(
         <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+            <UserPhoto
+                source={{ uri: 'https://github.com/larads.png'}}
+                alt=""
+                size={16}
+                mr={4}
+            />
             <VStack flex={1}>
                 <Text color="gray.100" fontSize="md">Olá</Text>
 
@@ -14,7 +21,7 @@ export function HomeHeader() {
             </VStack>
 
             <TouchableOpacity>
-                <Ionicons name="exit-outline" size={30} color="gray.200" />
+                <Ionicons name="exit-outline" size={30} color="gray.100" />
             </TouchableOpacity>
         </HStack>
     )
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,26 @@
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { Center } from 'native-base'
+import { HStack, VStack } from 'native-base'
+import { useState } from 'react'
 
 export function Home() {
+    const [groupSelected, setGroupSelected] = useState('costas')
+    
     return(
-        <HomeHeader />
+        <VStack flex={1}>
+            <HomeHeader />
+            <HStack>
+                <Group 
+                    name='costas' 
+                    isActive={groupSelected === 'costas'} 
+                    onPress={() => setGroupSelected('costas')}
+                />
+                <Group 
+                    name='ombro' 
+                    isActive={groupSelected === 'ombro'} 
+                    onPress={() => setGroupSelected('ombro')}
+                />
+            </HStack>
+        </VStack>
     )
 }
